5587bd41bf5b59e7c54997eb4da4f09f002d78a9
JoxanF <joxan@estudiantec.cr>
2022-10-14 17:08:11 -0600

    kmeans editado
    se añadieron los comentarios de medición analítica en el kmeans

--- kmeans/kmeans.py
+++ kmeans/kmeans.py
@@ -10,76 +10,79 @@ import matplotlib.pyplot as plt
 #Defining our function 
 # assigns, comparisons, executed lines
 def kmeans(x,k, no_of_iterations, measurementData):
-    idx = np.random.choice(len(x), k, replace=False)
+    idx = np.random.choice(len(x), k, replace=False) # +1
     #Randomly choosing Centroids 
-    centroids = x[idx, :] #Step 1
+    centroids = x[idx, :] #Step 1 +1
      
     #finding the distance between centroids and all the data points
-    distances = cdist(x, centroids ,'euclidean') #Step 2
+    distances = cdist(x, centroids ,'euclidean') #Step 2  +1
      
     #Centroid with the minimum Distance
-    points = np.array([np.argmin(i) for i in distances]) #Step 3
+    points = np.array([np.argmin(i) for i in distances]) #Step 3 +1
 
     # measurement
-    measurementData[0] += 4
-    measurementData[2] += 4
+    measurementData[0] += 4 
+    measurementData[2] += 4 
     # measurement
 
     #Repeating the above steps for a defined number of iterations
     #Step 4
-    for _ in range(no_of_iterations): 
-        centroids = []
-        for idx in range(k):
+    for _ in range(no_of_iterations): # +n
+        centroids = [] #+n
+        for idx in range(k): #+n^2
             #Updating Centroids by taking mean of Cluster it belongs to
-            temp_cent = x[points==idx].mean(axis=0) 
-            centroids.append(temp_cent)
+            temp_cent = x[points==idx].mean(axis=0) #+n^2
+            centroids.append(temp_cent) #+n^2
             # measurement
-            measurementData[0] += 3
-            measurementData[2] += 3
+            measurementData[0] += 3 
+            measurementData[2] += 3 
             # measurement
  
-        centroids = np.vstack(centroids) #Updated Centroids 
+        centroids = np.vstack(centroids) #Updated Centroids #+n
          
-        distances = cdist(x, centroids ,'euclidean')
-        points = np.array([np.argmin(i) for i in distances])
+        distances = cdist(x, centroids ,'euclidean') #+n
+        points = np.array([np.argmin(i) for i in distances]) #+n
 
         # measurement
-        measurementData[0] += 5
+        measurementData[0] += 5 
         measurementData[2] += 5
         # measurement
 
     # measurement
     measurementData[0] += 1
-    measurementData[2] += 1
+    measurementData[2] += 1 
     # measurement
-    return points 
+    return points #+1
+
+    # +1+1+1+1+n+n+n^2+n^2+n^2+n+n+n+1 = 5+5n+3n^2
+
  
  
 #Load Data
 #data = load_digits().data#
 
 # evaluate algorithm set for these values [10, 50, 100, 200, 500, 1000, 5000, 10000]
-sizesOfSets = [10, 50, 100, 200, 500, 1000, 5000, 10000]
+sizesOfSets = [10, 50, 100, 200, 500, 1000, 5000, 10000] 
 
-for size in sizesOfSets:
-    center_1 = np.array([1,1])
-    center_2 = np.array([5,5])
-    center_3 = np.array([8,1])
+for size in sizesOfSets: 
+    center_1 = np.array([1,1]) 
+    center_2 = np.array([5,5]) 
+    center_3 = np.array([8,1]) 
 
     # Generate random data and center it to the three centers
-    cluster_1 = np.random.randn(size, 2) + center_1
+    cluster_1 = np.random.randn(size, 2) + center_1 
     cluster_2 = np.random.randn(size,2) + center_2
-    cluster_3 = np.random.randn(size,2) + center_3
+    cluster_3 = np.random.randn(size,2) + center_3 
 
-    data = np.concatenate((cluster_1, cluster_2, cluster_3), axis = 0)
+    data = np.concatenate((cluster_1, cluster_2, cluster_3), axis = 0) 
     
     # declaring the list that will save assigns, comparisons, executed lines
-    measurementData = [0, 0, 0]
+    measurementData = [0, 0, 0] 
     # Applying our function
     # measurement of execution time of desicion tree
-    startTime = time.time()
-    label = kmeans(data,3,size, measurementData)
-    executionTime = (time.time() - startTime)
+    startTime = time.time() #+1
+    label = kmeans(data,3,size, measurementData) 
+    executionTime = (time.time() - startTime) 
     # ##############################################
     print(f'\nsize of set: {size}')
     print(f'tiempo de ejecución en (s): {executionTime}')
@@ -92,4 +95,3 @@ for size in sizesOfSets:
 #     plt.scatter(data[label == i , 0] , data[label == i , 1] , label = i)
 # plt.legend()
 # plt.show()
-
